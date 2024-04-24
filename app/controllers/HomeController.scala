4951895a7f07266d4726fa93262522b6f201c73a
Sebastian2811722 <94638117+PessimisticSodaCan@users.noreply.github.com>
2024-04-24 17:03:52 -0500

    data_compatability

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -4,6 +4,7 @@ import javax.inject._
 import play.api._
 import play.api.mvc._
 
+
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
@@ -19,6 +20,22 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
    * a path of `/`.
    */
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+   
+   val courses: Array[Map[String, Any]] = Array( 
+        Map("name" -> "1370", "sections" -> Array(Map("id" -> 1, "instructor" -> 12), Map("id" -> 2, "instructor" -> 2))),
+        Map("name" -> "3329", "sections" -> Array(Map("id" -> 3, "instructor" -> 7), Map("id" -> 4, "instructor" -> 31), Map("id" -> 5, "instructor" -> 31))),
+        Map("name" -> "3340", "sections" -> Array(Map("id" -> 6, "instructor" -> 23), Map("id" -> 7, "instructor" -> 3)))
+    )
+
+    val instructors: Array[Map[String, Any]] = Array(
+        Map("id" -> 12, "name" -> "Ayati"),
+        Map("id" -> 7, "name" -> "Gao"),
+        Map("id" -> 2, "name" -> "Kim"),
+        Map("id" -> 23, "name" -> "Schweller"),
+        Map("id" -> 31, "name" -> "Tomai"),
+        Map("id" -> 3, "name" -> "Wylie")
+    )
+
+    Ok(views.html.index(courses, instructors))
   }
 }
