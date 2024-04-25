3ca7a52aae75e9e08a74f5b9aea75d24e6f1831b
Sebastian2811722 <94638117+PessimisticSodaCan@users.noreply.github.com>
2024-04-24 22:29:21 -0500

    database compatability

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -4,38 +4,79 @@ import javax.inject._
 import play.api._
 import play.api.mvc._
 
+import play.api.db.Database
+import scala.collection.mutable.ArrayBuffer
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
-@Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+ 
+case class Course(id: Int, name: String, sections: Seq[Section])
+case class Section(id: Int, instructorId: Int)
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+
+@Singleton
+class HomeController @Inject()(val controllerComponents: ControllerComponents, db: Database) extends BaseController {
+    val instructors: Array[Map[String, Any]] = Array(
+      Map("id" -> 12, "name" -> "Ayati"),
+      Map("id" -> 7, "name" -> "Gao"),
+      Map("id" -> 2, "name" -> "Kim"),
+      Map("id" -> 23, "name" -> "Schweller"),
+      Map("id" -> 31, "name" -> "Tomai"),
+      Map("id" -> 3, "name" -> "Wylie")
+  )
+  
   def index() = Action { implicit request: Request[AnyContent] =>
    
-   val courses: Array[Map[String, Any]] = Array( 
-        Map("name" -> "1370", "sections" -> Array(Map("id" -> 1, "instructor" -> 12), Map("id" -> 2, "instructor" -> 2))),
-        Map("name" -> "3329", "sections" -> Array(Map("id" -> 3, "instructor" -> 7), Map("id" -> 4, "instructor" -> 31), Map("id" -> 5, "instructor" -> 31))),
-        Map("name" -> "3340", "sections" -> Array(Map("id" -> 6, "instructor" -> 23), Map("id" -> 7, "instructor" -> 3)))
-    )
+    val courses1 = getCourses(db)
+    println(courses1)
 
-    val instructors: Array[Map[String, Any]] = Array(
-        Map("id" -> 12, "name" -> "Ayati"),
-        Map("id" -> 7, "name" -> "Gao"),
-        Map("id" -> 2, "name" -> "Kim"),
-        Map("id" -> 23, "name" -> "Schweller"),
-        Map("id" -> 31, "name" -> "Tomai"),
-        Map("id" -> 3, "name" -> "Wylie")
-    )
+    Ok(views.html.index(courses1, instructors))
+  }
 
-    Ok(views.html.index(courses, instructors))
+ // Method to retrieve courses with their associated sections from the database
+
+private def getCourses(db: Database): Seq[Course] = {
+  db.withConnection { conn =>
+    val query = "SELECT * FROM course"
+    val stmt = conn.createStatement()
+    val rs = stmt.executeQuery(query)
+    
+    val courses = ArrayBuffer[Course]() // Use ArrayBuffer for mutable collection
+
+    while (rs.next()) {
+      val courseId = rs.getInt("id")
+      val courseName = rs.getString("name")
+      val sections = getSectionsForCourse(courseId, db)
+
+      val course = Course(courseId, courseName, sections)
+      courses += course // Append to ArrayBuffer
+    }
+
+    courses.toSeq // Convert back to immutable Seq for return type
+  }
+}
+
+  // Method to retrieve sections associated with passed course
+  private def getSectionsForCourse(courseId: Int, db: Database): Seq[Section] = {
+    db.withConnection { conn =>
+      val query = "SELECT * FROM section WHERE course_id = ?"
+      val pstmt = conn.prepareStatement(query)
+      pstmt.setInt(1, courseId)
+
+      val rs = pstmt.executeQuery()
+
+      val sections = ArrayBuffer[Section]()
+
+      while (rs.next()) {
+        val sectionId = rs.getInt("id")
+        val instructorId = rs.getInt("instructor_id")
+        val section = Section(sectionId, instructorId)
+        sections += section
+      }
+
+      sections.toSeq
+    }
   }
 }
--- build.sbt
+++ build.sbt
@@ -10,8 +10,14 @@ scalaVersion := "2.13.13"
 libraryDependencies += guice
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "7.0.0" % Test
 
+libraryDependencies += "org.xerial" % "sqlite-jdbc" % "3.36.0.1"
+libraryDependencies += jdbc
+
 // Adds additional packages into Twirl
 //TwirlKeys.templateImports += "com.edu.utrgv.controllers._"
+
+//libraryDependencies += "com.typesafe.play" %% "play-slick" % "5.0.0"
+//libraryDependencies += "com.typesafe.play" %% "play-sql" % "2.8.11"
 
 // Adds additional packages into conf/routes
 // play.sbt.routes.RoutesKeys.routesImport += "com.edu.utrgv.binders._"
