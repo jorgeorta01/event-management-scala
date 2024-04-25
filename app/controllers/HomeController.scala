9621edfd0152d00d53ab443e14a51e4a06a25f61
Sebastian2811722 <94638117+PessimisticSodaCan@users.noreply.github.com>
2024-04-25 03:19:23 -0500

    complete

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -19,6 +19,7 @@ case class Section(id: Int, instructorId: Int)
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents, db: Database) extends BaseController {
     val instructors: Array[Map[String, Any]] = Array(
+      Map("id" -> 1, "name" -> "Figueroa"),
       Map("id" -> 12, "name" -> "Ayati"),
       Map("id" -> 7, "name" -> "Gao"),
       Map("id" -> 2, "name" -> "Kim"),
@@ -29,34 +30,159 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents, d
   
   def index() = Action { implicit request: Request[AnyContent] =>
    
-    val courses1 = getCourses(db)
-    println(courses1)
+    val courses = getCourses(db)
 
-    Ok(views.html.index(courses1, instructors))
+    Ok(views.html.index(courses, instructors))
   }
 
- // Method to retrieve courses with their associated sections from the database
-
-private def getCourses(db: Database): Seq[Course] = {
-  db.withConnection { conn =>
-    val query = "SELECT * FROM course"
-    val stmt = conn.createStatement()
-    val rs = stmt.executeQuery(query)
+  def addInstructor() = Action { request =>
+    // Extract instructor ID from form data
+    val instructorIdOpt = request.body.asFormUrlEncoded.flatMap(_.get("instructorId").flatMap(_.headOption))
     
-    val courses = ArrayBuffer[Course]() // Use ArrayBuffer for mutable collection
+    // Extract course ID from form data
+    val courseIdOpt = request.body.asFormUrlEncoded.flatMap(_.get("courseId").flatMap(_.headOption))
 
-    while (rs.next()) {
-      val courseId = rs.getInt("id")
-      val courseName = rs.getString("name")
-      val sections = getSectionsForCourse(courseId, db)
+    // If both instructor ID and course ID are present, proceed
+    (instructorIdOpt, courseIdOpt) match {
+        case (Some(instructorId), Some(courseId)) =>
+            val instructorIdInt = instructorId.toInt
+            val courseIdInt = courseId.toInt
+            
+            // Call addSection function with both IDs
+            addSection(db, instructorIdInt, courseIdInt)
 
-      val course = Course(courseId, courseName, sections)
-      courses += course // Append to ArrayBuffer
+            val courses = getCourses(db)
+            println("Added instructor to section")
+
+            Ok(views.html.index(courses, instructors))
+        
+        case _ =>
+            // Handle the case where either instructor ID or course ID is missing
+            BadRequest("Missing instructor ID or course ID")
+    }
+  }
+
+  def addCourse() = Action { request =>
+
+    // Extract course ID from form data
+    val newCourse = request.body.asFormUrlEncoded.flatMap(_.get("addCourse").flatMap(_.headOption))
+
+    // If both instructor ID and course ID are present, proceed
+    (newCourse) match {
+        case (Some(courseId)) =>
+   
+            // Call addSection function with both IDs
+            addCourseToDatabase(courseId)
+
+            val courses = getCourses(db)
+            println("Added instructor to section")
+
+            Ok(views.html.index(courses, instructors))
+        
+        case _ =>
+            // Handle the case where either instructor ID or course ID is missing
+            BadRequest("Missing instructor ID or course ID")
+    }
+  }
+
+  def changeSection() = Action { request =>
+      val sectionIdFromForm = request.body.asFormUrlEncoded.flatMap(_.get("id").flatMap(_.headOption)).map(_.toInt)
+
+      val instructorIdOpt = request.body.asFormUrlEncoded.flatMap(_.get("instructorId").flatMap(_.headOption))
+
+          (instructorIdOpt, sectionIdFromForm) match {
+          case (Some(iId), Some(sId)) =>
+              val instructorIdInt = iId.toInt
+              val sectionIdInt = sId.toInt
+              
+              changeSectionFromDatabase(sectionIdInt, instructorIdInt)
+
+              val courses = getCourses(db)
+              println("Added instructor to section")
+
+              Ok(views.html.index(courses, instructors))
+          
+          case _ =>
+              // Handle the case where either instructor ID or course ID is missing
+              BadRequest("Missing instructor ID or course ID")
+      }
+  }
+
+
+  def deleteSection() = Action { request =>
+    val sectionIdFromForm = request.body.asFormUrlEncoded.flatMap(_.get("id").flatMap(_.headOption)).map(_.toInt)
+    sectionIdFromForm.foreach { id =>
+      deleteSectionFromDatabase(id)
     }
 
-    courses.toSeq // Convert back to immutable Seq for return type
+  
+    val courses = getCourses(db)
+
+    Ok(views.html.index(courses, instructors))
   }
-}
+    
+
+  private def changeSectionFromDatabase(sectionId: Int, instructorId: Int): Unit = {
+    db.withConnection { conn =>
+      val updateStatement = conn.prepareStatement(s"UPDATE section SET instructor_id = $instructorId WHERE id = $sectionId")
+      updateStatement.executeUpdate()
+    }
+  }
+    
+  // Method to delete a section from the database based on its ID
+  private def deleteSectionFromDatabase(sectionId: Int): Unit = {
+    db.withConnection { conn =>
+      val deleteStatement = conn.prepareStatement(s"DELETE FROM section WHERE id = $sectionId")
+      deleteStatement.executeUpdate()
+    }
+  }
+    
+  
+ // Method to retrieve courses with their associated sections from the database
+  
+  private def addSection(db: Database, Iid: Int, Cid: Int): Unit = {
+    db.withConnection { conn =>
+      val query = s"INSERT INTO section (course_id, instructor_id) VALUES ($Cid, $Iid)"
+      val stmt = conn.createStatement()
+      stmt.executeUpdate(query)
+    }
+  }
+
+  private def addCourseToDatabase(cId: String): Unit = {
+    db.withConnection { conn =>
+      val query = s"INSERT INTO course (name) VALUES ($cId)"
+      val stmt = conn.createStatement()
+      stmt.executeUpdate(query)
+    }
+  }
+
+  def getName(id: Int): String = {
+    instructors.find(_.get("id").contains(id)) match {
+      case Some(instructor) => instructor("name").toString
+      case None => "ID not found"
+    }
+  }
+
+  private def getCourses(db: Database): Seq[Course] = {
+    db.withConnection { conn =>
+      val query = "SELECT * FROM course"
+      val stmt = conn.createStatement()
+      val rs = stmt.executeQuery(query)
+      
+      val courses = ArrayBuffer[Course]() // Use ArrayBuffer for mutable collection
+
+      while (rs.next()) {
+        val courseId = rs.getInt("id")
+        val courseName = rs.getString("name")
+        val sections = getSectionsForCourse(courseId, db)
+
+        val course = Course(courseId, courseName, sections)
+        courses += course // Append to ArrayBuffer
+      }
+
+      courses.toSeq // Convert back to immutable Seq for return type
+    }
+  }
 
   // Method to retrieve sections associated with passed course
   private def getSectionsForCourse(courseId: Int, db: Database): Seq[Section] = {
